75160b7041eb1dda4209b7c742f27316b84fe62b
VishiK-on-github <vck281000@gmail.com>
2020-10-23 17:24:43 +0530

    switch updated

--- javascript/switchFunction/switchCitizen.js
+++ javascript/switchFunction/switchCitizen.js
@@ -1,9 +1,11 @@
+// Showing div citizen-complaint-status
 function showComplaintStatus() {
 
     document.getElementById("citizen-register-new-complaint").style.display = "none";
     document.getElementById("citizen-complaint-status").style.display = "block";
 }
 
+// Showing div citizen-register-new-complaint
 function showRegisterNewComplaint() {
 
     document.getElementById("citizen-complaint-status").style.display = "none";
---
+++ javascript/switchFunction/switchPolice.js
@@ -0,0 +1,13 @@
+// Showing div police-station-view
+function showViewComplaint() {
+
+    document.getElementById("police-station-view").style.display = "block";
+    document.getElementById("police-station-update-complaint-status").style.display = "none";
+}
+
+// Showing div police-station-update-complaint-status
+function showUpdateComplaintStatus() {
+
+    document.getElementById("police-station-update-complaint-status").style.display = "block";
+    document.getElementById("police-station-view").style.display = "none";
+}
