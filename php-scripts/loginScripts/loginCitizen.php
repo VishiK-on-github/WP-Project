0a834300c3852953f6f9ac688ff35ca92ed744b4
VishiK-on-github <vck281000@gmail.com>
2020-10-24 19:29:06 +0530

    changed successful login header path

--- php-scripts/loginScripts/loginCitizen.php
+++ php-scripts/loginScripts/loginCitizen.php
@@ -147,7 +147,7 @@
 			if($result0[0] == $user && $result0[1] == $pass) {
 
 				//echo "Success";
-				header("location: http://localhost/wp_project/WP-Project/User-Dashboards/citizenDashboard.php");
+				header("location: http://localhost/wp_project/WP-Project/User-Dashboards/citizenDashboard.html");
 			}
 			else {
 
