2536eed009c6c6cbae2f1f86ea140c65b14fb6d3
VishiK-on-github <vck281000@gmail.com>
2020-10-25 21:46:56 +0530

    header to php changed

--- php-scripts/loginScripts/loginCitizen.php
+++ php-scripts/loginScripts/loginCitizen.php
@@ -98,33 +98,6 @@
 
 			//echo "Connected Successfully";
 		}
-
-		// query to create citizen table
-		/*$sqltable = "CREATE TABLE IF NOT EXISTS citizen(
-			citizen_id INT PRIMARY KEY AUTO_INCREMENT,
-			firstname VARCHAR(70) NOT NULL,
-			lastname VARCHAR(70) NOT NULL,
-			username VARCHAR(30) NOT NULL UNIQUE,
-			email VARCHAR(50) NOT NULL UNIQUE,
-			pwd VARCHAR(50) NOT NULL UNIQUE,
-			addy VARCHAR(20) NOT NULL UNIQUE,
-			zipcode VARCHAR(6) NOT NULL,
-			contact VARCHAR(10) NOT NULL,
-			age VARCHAR(3) NOT NULL        
-		)";
-
-		if($conn->query($sqltable) === true){
-			echo "Table created successfully.";
-		} else{
-			echo "ERROR: Could not able to execute " . $conn->error;
-		}
-
-		$sql1="INSERT INTO citizen SET firstname='v', lastname='K', addy='123', zipcode=400000, username='Vishwanath10', pwd='Pass1234_*', email='someMail@mail.com', contact=0123456789, age=19";
-        if($conn->query($sql1) === true){
-            echo "Inserted into table successfully.";
-        } else{
-            echo "ERROR: Could not able to execute $sql1. " . $conn->error;
-		}*/
 		
 		// Setting the query to check the username and password
 		$sqlValid = "SELECT username, pwd FROM citizen WHERE username='$user'";
@@ -136,18 +109,11 @@
 
 		if($result != false) {
 
-			/*echo "Username : $result0[0] <br>";
-			echo "Password : $result0[1] <br>";
-			$var1 = $result0[0] == $user;
-			$var2 = $result0[1] == $pass;
-			echo "$var1";
-			echo "$var2";*/
-
 			// If username and password are matching we redirect to citizen dashboard
 			if($result0[0] == $user && $result0[1] == $pass) {
 
 				//echo "Success";
-				header("location: http://localhost/wp_project/WP-Project/User-Dashboards/citizenDashboard.html");
+				header("location: http://localhost/wp_project/WP-Project/User-Dashboards/citizenDashboard.php");
 			}
 			else {
 
