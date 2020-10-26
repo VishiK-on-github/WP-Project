7efb08d38d7dfc1c25888d47bd4f4747c6724638
VishiK-on-github <vck281000@gmail.com>
2020-10-27 01:55:20 +0530

    done

--- php-scripts/dashboardScripts/dashboardPolice.php
+++ php-scripts/dashboardScripts/dashboardPolice.php
@@ -16,19 +16,27 @@ if(isset($_POST["submit-update"])) {
     $conn = OpenCon(); //opening connection to server
     
     // get complaint id 
-    $complaint_id = trim($_POST["id"]);
+    $complaint_id = trim($_POST["complaint-id"]);
+    $complaint_desc_new = trim($_POST["new-status"] );
     
     // update the complaint table
-    $update_query = "UPDATE complaint SET complaint_status = '1' WHERE complaint_id = '$complaint_id' ";
+    $update_query = "UPDATE complaint SET complaint_status = '$complaint_desc_new' WHERE complaint_id = '$complaint_id' ";
 
     if($conn -> query($update_query) == true){
-        echo "Status has been updated <br>";
+        //echo "Status has been updated <br>";
+        header("location: http://localhost/wp_project/WP-Project/User-Dashboards/policeDashboard.php");
     }
     else {
         echo "ERROR: Query $update_query was unsuccessful <br>";
     }
 
     $conn -> close(); // close connection 
+}
+
+if(isset($_POST["sign-out"])) {
+
+    session_destroy();
+    header("location: http://localhost/wp_project/WP-Project/police_signin.html");
 }
 
 ?>
