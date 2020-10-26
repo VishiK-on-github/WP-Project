42d6f982547bb7a8a48d7933356e474251a24763
Arnav Verma <arnav2205@gmail.com>
2020-10-26 14:32:45 +0530

    added sessions

--- php-scripts/loginScripts/loginPolice.php
+++ php-scripts/loginScripts/loginPolice.php
@@ -3,6 +3,7 @@
 /*
 We are validating the login form for police
 */
+session_start();
 
 if(isset($_POST["submit"])){
 
@@ -16,35 +17,111 @@ if(isset($_POST["submit"])){
     // Validation of pid
     if(empty($pid)){
 
-        echo "Police ID is empty !!! <br>";
+        // echo "Police ID is empty !!! <br>";
         $sf1 = false;
     }
     elseif(!$valid_pid) {
 
-        echo "Police ID is valid <br>";
+        // echo "Police ID is invalid <br>";
+        $sf1 = false;
     }
     else{
 
-        echo "$pid is valid Police ID <br>";
+        // echo "$pid is valid Police ID <br>";
+        $sf1 = true;
     }
 
     // Validation of Password
     if(empty($password)){
 
-        echo "Password is empty !!! <br>";
+        // echo "Password is empty !!! <br>";
+        $sf2 = false;
     }
     elseif(!$valid_pass) {
 
-        echo "Password is valid <br>";
+        // echo "Password is invalid <br>";
+        $sf2 = false;
     }
     else{
 
-        echo "$password is valid Password <br>";
+        // echo "$password is valid Password <br>";
+        $sf2 = true;
+
+    }
+    if($sf1 == true && $sf2 == true){
+        validate($pid, $password);
+
     }
 }
 else{
 
-    echo "Something went wrong !!! <br>";
+    // echo "Something went wrong !!! <br>";
+    // redirect to error page 
+
+}
+
+function OpenConnection() {
+
+    # Method to create connection to the server
+
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "1234";
+    $db = "wp_project";
+    $conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
+
+    return $conn;
+}
+
+function validate($pid, $password){
+
+    $conn = OpenConnection();
+
+    // Creating session variable 
+    $_SESSION["pid"] = $pid;
+   
+    $location_query = "SELECT location FROM police_station WHERE police_id = '$pid' ";
+   
+    $location = $conn -> query($location_query);
+
+    $location0 = mysqli_fetch_array($location);
+
+    $_SESSION["location"] = $location0[0];
+    
+
+    if($conn == false) {
+
+        die("ERROR: Could not connect. " . $conn->connect_error);
+        // Display some error message
+    }
+    else {
+
+        //echo "Connected Successfully";
+    }
+
+    $sql = "SELECT police_id, pwd FROM police_station WHERE police_id = '$pid' ";
+
+    $result = $conn ->query($sql);
+
+    $result0 = mysqli_fetch_array($result);
+
+    if($result != false) {
+
+        if($result0[0] == $pid && $result0[1] == $password) {
+            // echo "success";
+            $conn -> close();
+            header("location: http://localhost/wp_project/WP-Project/User-Dashboards/policeDashboard.php");
+        }
+        else {
+
+            // Redirect to error page
+
+        }
+    }
+    else {
+        // echo "Invalid"
+        // Redirect to error page 
+    }
 }
 
 ?>
